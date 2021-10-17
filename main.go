2b73bd5f9b0b99051f0a25d8232c9bc04bb857ad
octo <dlqhdgkr456@gmail.com>
2021-10-17 20:38:58 +0900

    4.2 extract JOB part One

--- main.go
+++ main.go
@@ -9,6 +9,14 @@ import (
 	"github.com/PuerkitoBio/goquery"
 )
 
+type extractedJob struct {
+	id       string
+	title    string
+	location string
+	salary   string
+	summary  string
+}
+
 var baseURL string = "https://kr.indeed.com/jobs?q=python"
 
 func main() {
@@ -18,11 +26,35 @@ func main() {
 	}
 }
 
+// 각 page에서 정보를 가저오는 함수
 func getPage(page int) {
 	pageURL := baseURL + "&start=" + strconv.Itoa(page*10)
 	fmt.Println(pageURL)
+	response, err := http.Get(pageURL)
+	checkErr(err)
+	checkCode(response)
+
+	defer response.Body.Close()
+
+	document, err := goquery.NewDocumentFromReader(response.Body)
+	checkErr(err)
+
+	searchCards := document.Find(".tapItem ")
+	searchCards.Each(func(i int, card *goquery.Selection) {
+		id, _ := card.Attr("data-jk")
+		fmt.Println(id)
+		title := card.Find(".jobTitle").Text()
+		fmt.Println(title)
+		location := card.Find(".companyLocation").Text()
+		fmt.Println(location)
+	})
 }
 
+func cleanString(str string) string {
+
+}
+
+// page의 개수를 가져오는 함수
 func getPages() int {
 	pages := 0
 	response, err := http.Get(baseURL)
@@ -41,12 +73,14 @@ func getPages() int {
 	return pages
 }
 
+// error를 check하는 함수 (error가 발생하면 프로그램 종료)
 func checkErr(err error) {
 	if err != nil {
 		log.Fatalln(err)
 	}
 }
 
+// 제대로된 Response를 가져오는지 check하는 함수 (200이 아니면 프로그램 종료)
 func checkCode(response *http.Response) {
 	if response.StatusCode != 200 {
 		log.Fatalln("Request failed with Statues:", response.StatusCode)
